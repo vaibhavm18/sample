5c1d062f8452bcfaa281d7434104e400c6463c8f
vaibhav <vaibhavnayak018@gmail.com>
2024-06-13 23:22:29 +0530

    new dev branch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
 "use client";
-import Footer from "@/components/Footer";
-import Header from "@/components/Header";
-import Home from "@/components/Home";
-import PaymentModels from "@/components/Payment";
-import TestimonialSlider from "@/components/Testimonial";
+
 import { useEffect } from "react";
 
 export default function Page() {
@@ -15,13 +11,7 @@ export default function Page() {
   }, []);
   return (
     <div className="max-w-7xl mx-auto">
-      <Header />
-      <main className="pt-16 mx-4">
-        <Home />
-        <PaymentModels />
-        <TestimonialSlider />
-        <Footer />
-      </main>
+     
     </div>
   );
 }
